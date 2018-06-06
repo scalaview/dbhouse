4476e367367b4ce03071568ee8cb77004811baf9
scalaview <chailink100@gmail.com>
2018-06-06 18:14:24 +0800

    add noise

--- lstm.py
+++ lstm.py
@@ -6,6 +6,7 @@ import pandas as pd
 import seaborn as sns
 from sklearn.metrics import mean_absolute_error
 import utils
+import noise
 
 def build_lstm_model(input_data, output_size, neurons=20, activ_func='linear',
                      dropout=0.25, loss='mae', optimizer='adam'):
@@ -35,37 +36,39 @@ def prepare_train_data(syml):
     hist = pd.read_sql_query('SELECT `date`, open_price, high_price, low_price, close_price, volumeto, volumefrom FROM daily_prices WHERE fsymbol="'+syml+'" AND tsymbol="USDT"', models.engine)
     hist = hist.set_index('date')
     hist.index = pd.to_datetime(hist.index, unit='s')
-    hist = hist.join(pd.Series(hist['close_price'].shift(-1), name='look_back_1_close_price'))
+    hist = hist.join(pd.Series(noise.wt(hist, 'close_price', 'db4', 4, 2, 4), name='denoised_close_price', index=hist.index))
+    hist = hist.join(pd.Series(hist['denoised_close_price'].shift(-3), name='look_back_1_close_price'))
     return hist.fillna(0)
 
 def run_train(hist):
     np.random.seed(42)
     # data params
+    total = hist.shape[0]
     window_len = 7
     test_size = 0.02
     zero_base = True
     # model params
-    lstm_neurons = 20
-    epochs = 1000
-    batch_size = 7
+    lstm_neurons = 200
+    epochs = 500
+    batch_size = 500
     loss = 'mae'
-    dropout = 0.25
+    dropout = 0.0025
     optimizer = 'adam'
     target_col = 'look_back_1_close_price'
-    train, test, X_train, X_test, y_train, y_test = utils.prepare_data(hist, target_col, window_len=window_len, zero_base=zero_base, test_size=test_size)
+    train, test, X_train, X_test, y_train, y_test = utils.prepare_data(hist[['open_price', 'high_price', 'low_price', 'volumeto', 'volumefrom', 'denoised_close_price', 'look_back_1_close_price']], target_col, window_len=window_len, zero_base=zero_base, test_size=test_size)
     for x, y, z in np.argwhere(np.isnan(X_train)):
         X_train[(x, y, z)] = 0
     model = build_lstm_model(X_train, output_size=1, neurons=lstm_neurons, dropout=dropout, loss=loss, optimizer=optimizer)
     history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=1, shuffle=True)
-    targets = test['close_price'][window_len:]
     preds = model.predict(X_test).squeeze()
+    targets = hist['close_price'][total-preds.shape[0]:]
     maer = mean_absolute_error(preds, y_test)
     print("mean_absolute_error: %f" % maer)
     preds = test[target_col].values[:-window_len] * (preds + 1)
-    preds = pd.Series(index=(targets.index + pd.DateOffset(1)), data=preds)
+    preds = pd.Series(index=(targets.index + pd.DateOffset(3)), data=preds)
     line_plot(targets, preds, 'actual', 'prediction', lw=3)
     print(hist['close_price'].tail())
-    print(preds.tail())
+    print(preds.tail(13))
     n_points = 7
     line_plot(targets[-n_points:], preds[-n_points:], 'actual', 'prediction', lw=3)
 
---
+++ noise.py
@@ -0,0 +1,35 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+import pywt   # python 小波变换的包
+
+
+def wt(data, keyname, wavefunc, level, m, n):
+    """
+    小波降噪函数
+    - level: 分解层数；
+    - data: 保存列表类型的字典；
+    - keyname: 键名；
+    - index_list: 待处理序列；
+    - wavefunc: 选取的小波函数；
+    - m,n 选择进行阈值处理的小波系数层数
+    """
+    # 分解
+    coeff = pywt.wavedec(data[keyname], wavefunc, mode='sym', level=level)
+    # 设置 sgn 函数
+    sgn = lambda x: 1 if x > 0 else -1 if x < 0 else 0
+    # 降噪过程
+    for i in range(m, n + 1):  # 选取小波系数层数为 m~n 层
+        cD = coeff[i]
+        for j in range(len(cD)):
+            Tr = np.sqrt(2 * np.log(len(cD)))  # 计算阈值
+            if cD[j] >= Tr:
+                coeff[i][j] = sgn(cD[j]) - Tr  # 使用 sgn 函数向零收缩
+            else:
+                coeff[i][j] = 0  # 低于阈值置零
+    # 重新构建
+    denoised_index = pywt.waverec(coeff, wavefunc)
+    # 为了避免出现负值的情况，取绝对值
+    # abs_denoised_list = list(map(lambda x: abs(x), denoised_data_list))
+    # 返回降噪结果
+    return denoised_index[1:]
